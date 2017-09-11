96fdc81e1bc9994531e1e0e8eee617c9d54830b3
Andy Feng <andyfengc@gmail.com>
2017-09-10 23:57:26 -0400

    add react form demo

---
+++ reactjs/start/src/components/sample-form.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var SampleForm  = React.createClass({
+  render: function(){
+    return <form>
+      <input type="text" ></input>
+      <button>Submit</button>
+    </form>;
+  }
+})
+module.exports = SampleForm;
--- reactjs/start/src/main.jsx
+++ reactjs/start/src/main.jsx
@@ -1,5 +1,7 @@
 var React = require('react');
 var ReactDom = require('react-dom');
 var List = require('./components/list.jsx');
+var SampleForm = require('./components/sample-form.jsx');
 
 ReactDom.render(<List/>, document.getElementById('persons'));
+ReactDom.render(<SampleForm></SampleForm>, document.getElementById('sampleForm'));
